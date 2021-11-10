356e91747042b9ce2c83cefd3a01c148ab1ef4fc
Augusto <pbadillo86@hotmail.com>
2021-11-10 16:36:32 +0100

    modificacion mapa

--- AppMeteorologia/src/Vista/VistaPrincipal.java
+++ AppMeteorologia/src/Vista/VistaPrincipal.java
@@ -44,65 +44,6 @@ public class VistaPrincipal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel_9 = new JLabel("Dublin");
-		lblNewLabel_9.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_9.setForeground(new Color(255, 0, 0));
-		lblNewLabel_9.setBounds(115, 222, 46, 14);
-		contentPane.add(lblNewLabel_9);
-		
-		JLabel lblNewLabel_8 = new JLabel("Estocolmo");
-		lblNewLabel_8.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_8.setForeground(new Color(255, 0, 0));
-		lblNewLabel_8.setBounds(292, 177, 79, 14);
-		contentPane.add(lblNewLabel_8);
-		
-		JLabel lblNewLabel_7 = new JLabel("Lisboa");
-		lblNewLabel_7.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_7.setForeground(new Color(255, 0, 0));
-		lblNewLabel_7.setBounds(65, 377, 46, 14);
-		contentPane.add(lblNewLabel_7);
-		
-		JLabel lblNewLabel_6 = new JLabel("Berlin");
-		lblNewLabel_6.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_6.setForeground(new Color(255, 0, 0));
-		lblNewLabel_6.setBounds(306, 256, 46, 14);
-		contentPane.add(lblNewLabel_6);
-		
-		JLabel lblNewLabel_5 = new JLabel("Londres");
-		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_5.setForeground(new Color(255, 0, 0));
-		lblNewLabel_5.setBounds(166, 236, 66, 14);
-		contentPane.add(lblNewLabel_5);
-		
-		JLabel lblNewLabel_4 = new JLabel("Atenas");
-		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_4.setForeground(new Color(255, 0, 0));
-		lblNewLabel_4.setBounds(380, 407, 59, 14);
-		contentPane.add(lblNewLabel_4);
-		
-		JLabel lblNewLabel_3 = new JLabel("Roma");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_3.setForeground(new Color(255, 0, 0));
-		lblNewLabel_3.setBounds(294, 377, 46, 14);
-		contentPane.add(lblNewLabel_3);
-		
-		JLabel lblNewLabel_2 = new JLabel("Paris");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_2.setForeground(new Color(255, 0, 0));
-		lblNewLabel_2.setBounds(206, 307, 46, 14);
-		contentPane.add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_1 = new JLabel("Madrid");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_1.setForeground(new Color(255, 0, 0));
-		lblNewLabel_1.setBounds(128, 389, 46, 14);
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setBounds(48, 57, 522, 384);
-		contentPane.add(lblNewLabel);
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Patricio\\git\\App_Meteorologica\\App_Metereologica\\Images\\fisico-europa1.PNG"));
-		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(700, 68, 176, 31);
 	contentPane.add(comboBox);
@@ -138,7 +79,67 @@ public class VistaPrincipal extends JFrame {
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new TitledBorder(null, "Mapa", TitledBorder.LEADING, TitledBorder.TOP, null, Color.RED));
-		panel_2.setBounds(42, 41, 538, 407);
+		panel_2.setBounds(42, 36, 538, 407);
 		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel_4 = new JLabel("Amsterdam");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_4.setForeground(new Color(255, 0, 0));
+		lblNewLabel_4.setBounds(271, 185, 92, 16);
+		panel_2.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_1 = new JLabel("Madrid");
+		lblNewLabel_1.setBounds(172, 346, 44, 16);
+		panel_2.add(lblNewLabel_1);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_1.setForeground(new Color(255, 0, 0));
+		
+		JLabel lblNewLabel_7 = new JLabel("Lisboa");
+		lblNewLabel_7.setBounds(86, 361, 46, 16);
+		panel_2.add(lblNewLabel_7);
+		lblNewLabel_7.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_7.setForeground(new Color(255, 0, 0));
+		
+		JLabel lblNewLabel_2 = new JLabel("Paris");
+		lblNewLabel_2.setBounds(260, 267, 46, 14);
+		panel_2.add(lblNewLabel_2);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_2.setForeground(new Color(255, 0, 0));
+		
+		JLabel lblNewLabel_3 = new JLabel("Roma");
+		lblNewLabel_3.setBounds(319, 362, 46, 14);
+		panel_2.add(lblNewLabel_3);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_3.setForeground(new Color(255, 0, 0));
+		
+		JLabel lblNewLabel_5 = new JLabel("Londres");
+		lblNewLabel_5.setBounds(195, 198, 66, 14);
+		panel_2.add(lblNewLabel_5);
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_5.setForeground(new Color(255, 0, 0));
+		
+		JLabel lblNewLabel_9 = new JLabel("Dublin");
+		lblNewLabel_9.setBounds(148, 168, 46, 14);
+		panel_2.add(lblNewLabel_9);
+		lblNewLabel_9.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_9.setForeground(new Color(255, 0, 0));
+		
+		JLabel lblNewLabel_6 = new JLabel("Berlin");
+		lblNewLabel_6.setBounds(365, 228, 46, 14);
+		panel_2.add(lblNewLabel_6);
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_6.setForeground(new Color(255, 0, 0));
+		
+		JLabel lblNewLabel_8 = new JLabel("Estocolmo");
+		lblNewLabel_8.setBounds(412, 93, 79, 14);
+		panel_2.add(lblNewLabel_8);
+		lblNewLabel_8.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_8.setForeground(new Color(255, 0, 0));
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(10, 23, 518, 373);
+		panel_2.add(lblNewLabel);
+		lblNewLabel.setIcon(new ImageIcon("../AppMeteorologia/AppMeteorologia/imagenes/1.jpg"));
 	}
 }
